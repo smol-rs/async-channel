06611d43ad0c44b198785421b40718021b7c165a
John Nunley <jtnunley01@gmail.com>
2023-07-16 21:25:43 -0700

    Add smol-rs logo

--- src/lib.rs
+++ src/lib.rs
@@ -28,6 +28,12 @@
 
 #![forbid(unsafe_code)]
 #![warn(missing_docs, missing_debug_implementations, rust_2018_idioms)]
+#![doc(
+    html_favicon_url = "https://raw.githubusercontent.com/smol-rs/smol/master/assets/images/logo_fullsize_transparent.png"
+)]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/smol-rs/smol/master/assets/images/logo_fullsize_transparent.png"
+)]
 
 use std::error;
 use std::fmt;
